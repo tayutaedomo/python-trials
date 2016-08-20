abfb1fc44440c17560bea1e380f6d1443f5ca4b1
tayutaedomo <tayutaedomo@gmail.com>
2016-08-20 16:29:25 +0900

    Add script template

---
+++ scripts/script_template.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Refer
+#  - http://d.hatena.ne.jp/s-n-k/20080512/1210611374
+#  - http://d.hatena.ne.jp/s-n-k/20080513/1210692988
+if __name__ == '__main__':
+    print 'スクリプトの時だけ  __name__ = %s' % __name__
+
