0557cdeba5223123133a067d315916050aec3768
tayutaedomo <tayutaedomo@gmail.com>
2020-11-17 16:20:51 +0900

    Bugfix of 8x8 fonts

--- scripts/pil/dot_char.py
+++ scripts/pil/dot_char.py
@@ -65,7 +65,7 @@ class DotCharConverter:
 
 class CharPos8x8:
     NUMERICS = [
-        '0', '1', '3', '4', '5', '6', '7', '8', '9'
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
     ]
     UPPER_CHARS = [
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
