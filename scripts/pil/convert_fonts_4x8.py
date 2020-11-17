6f61c924c867e55c87c83194ac5f3327c3703a7e
tayutaedomo <tayutaedomo@gmail.com>
2020-11-17 16:41:40 +0900

    Add pil/convert_fonts_4x8.py

---
+++ scripts/pil/convert_fonts_4x8.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import json
+
+ROOT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..')
+ROOT_PATH = os.path.abspath(ROOT_PATH)
+sys.path.append(os.path.abspath(ROOT_PATH))
+
+from scripts.pil.dot_char import Font4x8Converter 
+
+
+if __name__ == '__main__':
+    file_path = sys.argv[1]
+    converter = Font4x8Converter(file_path)
+    conv_all = converter.convert_all()
+
+    dest_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'fonts_4x8.json')
+    with open(dest_path, 'w') as f:
+        json.dump(conv_all, f)
+
+    print('Exported successfully.', dest_path)
