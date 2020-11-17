2085da1f2b78da37ed1c1f1d9596fbc2630b8d33
tayutaedomo <tayutaedomo@gmail.com>
2020-11-17 16:43:55 +0900

    Rename convert_fonts_8x8.py

--- scripts/pil/convert_fonts_8x8.py
+++ scripts/pil/convert_fonts_8x8.py
@@ -12,11 +12,9 @@ from scripts.pil.dot_char import DotCharConverter
 if __name__ == '__main__':
     file_path = sys.argv[1]
     converter = DotCharConverter(file_path)
-
     conv_all = converter.convert_all()
-    # print(conv_all)
 
-    dest_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'dot_char.json')
+    dest_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'fonts_8x8.json')
     with open(dest_path, 'w') as f:
         json.dump(conv_all, f)
 
